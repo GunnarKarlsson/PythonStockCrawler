866ed7d6aa5f8e079f0c129111105116a5ccee6e
gunnarkarlsson <gunnar.h.karlsson@gmail.com>
2019-05-04 11:14:03 +0800

    Crawl current ratio

--- crawl_fundamentals.py
+++ crawl_fundamentals.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import time
+import re
 #import pprint
 
 #pp = pprint.PrettyPrinter(indent=4)
@@ -14,16 +15,31 @@ def save_details(soup, collection):
         dict = {}
         dict.update({"time":t})
 
-        #date_headers = soup.find_all(attrs={"ref" : "FR_Field_NB_1"})
+        find_elements("Closing Date")
+        try:
+            r = soup.find_all(text=re.compile("Current Ratio(.*)"));
+            print(r);
+        except:
+            pass
 
-        closing_dates = soup.find("td", text="Closing Date").find_next_siblings("td")
+        try:
+            find_elements("\rCurrent Ratio (X)\r")
+        except:
+            pass
 
-        print("closing dates: ",closing_dates)
-        for n in closing_dates:
-            print(n.text)
+        #r = soup.find_all(text=re.compile("Current Ratio(.*)"));
+        #print(r);
+
+def find_elements(name):
+    t = soup.find("td", text=name);
+    print("t: ", t)
+    t = t.find_next_siblings("td")
+    #print("name: ", t)
+    for n in t:
+        print(n.text)
 
 start = 1
-end = 1
+end = 10
 #client = pymongo.MongoClient("mongodb+srv://user0:asdfasdf@cluster0-813m4.mongodb.net/hkstocks?retryWrites=true")
 client = pymongo.MongoClient("mongodb://localhost:27017/hkstocks?retryWrites=true")
 db = client.hkstocks
