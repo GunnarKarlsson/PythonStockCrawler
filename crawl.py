821e8ec1a9d6c9d150108c7851cfaf08cf2f1997
gunnarkarlsson <gunnar.h.karlsson@gmail.com>
2019-06-22 16:41:59 +0800

    set range to 1 to 9999

--- crawl.py
+++ crawl.py
@@ -86,8 +86,8 @@ from string import printable
 
 #pp = pprint.PrettyPrinter(indent=4)
 
-start = 129
-end = 131
+start = 1
+end = 9999
 
 #client = pymongo.MongoClient("mongodb+srv://user0:asdfasdf@cluster0-813m4.mongodb.net/hkstocks?retryWrites=true")
 client = pymongo.MongoClient("mongodb://localhost:27017/hkstocks?retryWrites=true")
